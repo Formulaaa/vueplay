0a694d395be08f3d9bcea4031bae88c723441401
Formula <84565335+Formulaaa@users.noreply.github.com>
2022-09-17 12:21:07 +0800

    将路由与侧边栏的选项组以及其中的选项绑定

--- src/layout/SiderBar/Menu/SideMenu.vue
+++ src/layout/SiderBar/Menu/SideMenu.vue
@@ -4,33 +4,70 @@
  * @Author: congsir
  * @Date: 2022-09-16 19:02:46
  * @LastEditors: Please set LastEditors
- * @LastEditTime: 2022-09-16 20:55:16
+ * @LastEditTime: 2022-09-17 12:21:02
 -->
 <template>
     <a-menu v-model:selectedKeys="selectedKeys" v-model:openKeys="openKeys" mode="inline" @openChange="onOpenChange"
         :style="{ height: '100%', borderRight: 0 }" @click="clickItem">
-        <side-sub-menu v-for="item of items" :item="item" :key="item.path"></side-sub-menu>
+        <template v-for="item of items" :key="item.path">
+            <side-sub-menu :item="item" v-if="item.path!='/'"></side-sub-menu>
+        </template>
     </a-menu>
 </template>
 
 <script setup lang="ts">
-import { ref } from "vue"
+import { ref, computed } from "vue"
 import { useRouter } from 'vue-router'
 import SideSubMenu from './SideSubMenu.vue'
+// 声明Vue Router的两个对象
 const router = useRouter();
+// 获取/router/index.ts中声明的routes数组
 const items = router.options.routes;
-
+/**
+ * @name: clickItem
+ * @msg: 路由跳转
+ * @param {*} keyPath
+ * @return {*}
+ */
 const clickItem = ({ keyPath }: { keyPath: string[] }) => {
-  router.push(keyPath.join('/'));
+    router.push(keyPath.join('/'));
 }
+/**
+ * @name: selectedKeys
+ * @msg: 从路径绑定当前选中的侧边栏选项
+ * @param {*} computed
+ * @return {*}
+ */
+const selectedKeys = computed(() => {
+    let arr: string[] = [];
+    arr.push(router.currentRoute.value.path.split('/')[2]);
+    return arr;
+})
+// let selectedKeys = ref<string[]>('hello-world');
+/**
+ * @name: openKeys
+ * @msg: 从路径获取当前展开的侧边栏选项组
+ * @param {*} computed
+ * @return {*}
+ */
+const openKeys = computed(() => {
+    let arr: string[] = [];
+    arr.push('/' + router.currentRoute.value.path.split('/')[1]);
+    return arr;
+})
+// let openKeys = ref<string[]>('/home');
+
+/**
+ * @name: onOpenChange
+ * @msg: 只展开被选中选项的选项组，折叠其他选项组
+ * @param {*} nowOpenKeys
+ * @return {*}
+ */
+// const onOpenChange = (nowOpenKeys: string[]) => {
+//     openKeys.value = nowOpenKeys[nowOpenKeys.length - 1] ? [nowOpenKeys[nowOpenKeys.length - 1]] : [];
+// }
 
 
-let selectedKeys = ref<string[]>(['hello-world']);
-
-let openKeys = ref<string[]>(['/home']);
-const onOpenChange = (nowOpenKeys: string[]) => {
-    openKeys.value = nowOpenKeys[nowOpenKeys.length - 1] ? [nowOpenKeys[nowOpenKeys.length - 1]] : [];
-}
 </script>
 
 <style scoped>
--- src/router/index.ts
+++ src/router/index.ts
@@ -4,7 +4,7 @@
  * @Author: congsir
  * @Date: 2022-09-09 11:52:35
  * @LastEditors: Please set LastEditors
- * @LastEditTime: 2022-09-17 11:23:27
+ * @LastEditTime: 2022-09-17 12:07:25
  */
 import { createRouter, createWebHistory } from "vue-router";
 import HelloWorldVue from "@/views/basic/HelloWorld.vue";
@@ -13,6 +13,10 @@ import { UserOutlined } from "@ant-design/icons-vue";
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
+    {
+      path: '/',
+      redirect:'/home/hello-world'
+    },
     {
       path: "/home",
       name: "基础",
