5eaf2292c3dd86b319799e9f97ac97c447ca5452
Formula <84565335+Formulaaa@users.noreply.github.com>
2022-10-08 20:05:56 +0800

    markdown一级标题无法使用

--- src/views/exercise/MarkDown.vue
+++ src/views/exercise/MarkDown.vue
@@ -4,31 +4,44 @@
  * @Author: congsir
  * @Date: 2022-10-08 14:41:28
  * @LastEditors: Please set LastEditors
- * @LastEditTime: 2022-10-08 15:47:54
+ * @LastEditTime: 2022-10-08 16:09:06
 -->
 <template>
     <a-row :gutter="10">
-      <a-col :span="12">
-        <a-textarea v-model:value="input" autosize :bordered="false" @input="update" >
-        </a-textarea>
-      </a-col>
-      <a-col :span="12">
-        <div v-html="output"></div>
-      </a-col>
+        <a-col :span="12">
+            <a-textarea v-model:value="input" autosize :bordered="false">
+            </a-textarea>
+        </a-col>
+        <a-col :span="12">
+            <div v-html="output"></div>
+        </a-col>
     </a-row>
 </template>
 
 <script setup lang="ts">
-import { marked } from "marked"
-import { debounce } from 'lodash-es'
-import { ref,computed } from 'vue';
-import type { ChangeEvent } from "ant-design-vue/es/_util/EventInterface";
-
-const input = ref("## World");
-const output = computed(() => marked(input.value))
-const update = debounce((e:ChangeEvent) => {
-    input.value = e.target.value || ''
-},100)
+import {marked} from "marked"
+// import { debounce } from 'lodash-es'
+import { ref, computed } from 'vue';
+// import type { ChangeEvent } from "ant-design-vue/es/_util/EventInterface";
+// let rendererMD = new marked.Renderer();
+// marked.setOptions({
+//     renderer: rendererMD,
+//     gfm: true,
+//     tables: true,
+//     breaks: false,
+//     pedantic: false,
+//     sanitize: false,
+//     smartLists: true,
+//     smartypants: false
+// });
+const input = ref('## Hello');
+const output = computed(() => {
+    // console.log(input.value);
+    return marked(input.value);
+})
+// const update = debounce((e:ChangeEvent) => {
+//     input.value = e.target.value || ''
+// },100)
 
 </script>
 
